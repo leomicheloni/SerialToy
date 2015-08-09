d16c2f192a1a67e51aca46f8b669c952def158fe
Leonardo <leomicheloni@hotmail.com>
2015-08-09 13:33:38 -0300

    added buttons and text boxes

--- SerialToy/Form1.Designer.cs
+++ SerialToy/Form1.Designer.cs
@@ -28,12 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPortName = new System.Windows.Forms.TextBox();
+            this.btnOpenPort = new System.Windows.Forms.Button();
+            this.btnSendData = new System.Windows.Forms.Button();
+            this.txtDataToSend = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtPortName
+            // 
+            this.txtPortName.Location = new System.Drawing.Point(79, 37);
+            this.txtPortName.Name = "txtPortName";
+            this.txtPortName.Size = new System.Drawing.Size(100, 20);
+            this.txtPortName.TabIndex = 1;
+            // 
+            // btnOpenPort
+            // 
+            this.btnOpenPort.Location = new System.Drawing.Point(200, 37);
+            this.btnOpenPort.Name = "btnOpenPort";
+            this.btnOpenPort.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenPort.TabIndex = 2;
+            this.btnOpenPort.Text = "Open port";
+            this.btnOpenPort.UseVisualStyleBackColor = true;
+            // 
+            // btnSendData
+            // 
+            this.btnSendData.Location = new System.Drawing.Point(566, 35);
+            this.btnSendData.Name = "btnSendData";
+            this.btnSendData.Size = new System.Drawing.Size(75, 23);
+            this.btnSendData.TabIndex = 3;
+            this.btnSendData.Text = "Send";
+            this.btnSendData.UseVisualStyleBackColor = true;
+            // 
+            // txtDataToSend
+            // 
+            this.txtDataToSend.Location = new System.Drawing.Point(393, 39);
+            this.txtDataToSend.Name = "txtDataToSend";
+            this.txtDataToSend.Size = new System.Drawing.Size(167, 20);
+            this.txtDataToSend.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(746, 327);
+            this.Controls.Add(this.txtDataToSend);
+            this.Controls.Add(this.btnSendData);
+            this.Controls.Add(this.btnOpenPort);
+            this.Controls.Add(this.txtPortName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Serial Toy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPortName;
+        private System.Windows.Forms.Button btnOpenPort;
+        private System.Windows.Forms.Button btnSendData;
+        private System.Windows.Forms.TextBox txtDataToSend;
     }
 }
 
--- SerialToy/Form1.cs
+++ SerialToy/Form1.cs
@@ -16,5 +16,10 @@ namespace SerialToy
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
